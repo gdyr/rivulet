6ebc6275d04ea51a132478eea349ebfb69b881d0
Michael G <gmichael225@gmail.com>
2018-01-18 11:42:22 +1100

    Initial commit

---
+++ Rivulet/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Rivulet
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.stream = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.id = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.address = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.packets = new System.Windows.Forms.TextBox();
+            this.dropped = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.stream);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 52);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Stream ID";
+            // 
+            // stream
+            // 
+            this.stream.Location = new System.Drawing.Point(7, 20);
+            this.stream.Name = "stream";
+            this.stream.Size = new System.Drawing.Size(187, 20);
+            this.stream.TabIndex = 0;
+            this.stream.TextChanged += new System.EventHandler(this.stream_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.id);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.address);
+            this.groupBox2.Location = new System.Drawing.Point(13, 71);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 78);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Multicast";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ID";
+            // 
+            // id
+            // 
+            this.id.Location = new System.Drawing.Point(66, 46);
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            this.id.Size = new System.Drawing.Size(128, 20);
+            this.id.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Address";
+            // 
+            // address
+            // 
+            this.address.Location = new System.Drawing.Point(66, 20);
+            this.address.Name = "address";
+            this.address.ReadOnly = true;
+            this.address.Size = new System.Drawing.Size(128, 20);
+            this.address.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Location = new System.Drawing.Point(12, 155);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 56);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Controls";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Lime;
+            this.button2.Location = new System.Drawing.Point(119, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Play";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Record";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Controls.Add(this.packets);
+            this.groupBox4.Controls.Add(this.dropped);
+            this.groupBox4.Controls.Add(this.label4);
+            this.groupBox4.Location = new System.Drawing.Point(13, 217);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(200, 76);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Packets";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Dropped";
+            // 
+            // packets
+            // 
+            this.packets.Location = new System.Drawing.Point(65, 19);
+            this.packets.Name = "packets";
+            this.packets.ReadOnly = true;
+            this.packets.Size = new System.Drawing.Size(129, 20);
+            this.packets.TabIndex = 4;
+            // 
+            // dropped
+            // 
+            this.dropped.Location = new System.Drawing.Point(65, 45);
+            this.dropped.Name = "dropped";
+            this.dropped.ReadOnly = true;
+            this.dropped.Size = new System.Drawing.Size(129, 20);
+            this.dropped.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Received";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(225, 303);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
+            this.Text = "Rivulet";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox stream;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox id;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox address;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox packets;
+        private System.Windows.Forms.TextBox dropped;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Rivulet/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+using System.Threading;
+using NAudio.Wave;
+using System.Timers;
+
+namespace Rivulet
+{
+    public partial class MainWindow : Form
+    {
+
+        Thread udpWorker;
+        IPAddress mIP;
+
+        bool listen = false;
+        int lastPacketID = -2;
+
+        bool playing = false;
+        BufferedWaveProvider soundBuffer;
+        WaveOut sound;
+
+        int packetCount = 0;
+        int dropCount = 0;
+
+        System.Timers.Timer countDisplayTimer;
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            setupAudio();
+            setupStream();
+
+            // Set up display timer
+            countDisplayTimer = new System.Timers.Timer(100);
+            countDisplayTimer.Elapsed += new ElapsedEventHandler(displayCounts);
+            countDisplayTimer.Start();
+
+            this.FormClosed += new FormClosedEventHandler(cleanExit);
+
+        }
+
+        private void cleanExit(object sender, FormClosedEventArgs e)
+        {
+            packupStream();
+            Application.Exit();
+        }
+
+        private void displayCounts(object sender, ElapsedEventArgs args)
+        {
+            if(!listen) { return; }
+            SafeSet(packetCount.ToString(), packets);
+            SafeSet(dropCount.ToString(), dropped);
+        }
+
+        private void setupAudio()
+        {
+            soundBuffer = new BufferedWaveProvider(new WaveFormat(48000, 16, 2));
+            sound = new WaveOut();
+            sound.Init(soundBuffer);
+        }
+
+        private void stream_TextChanged(object sender, EventArgs e)
+        {
+            setupStream();
+        }
+
+        private void setupStream()
+        {
+
+            packupStream();
+
+            // Be patient with users that haven't typed anything yet
+            if(stream.Text.Length < 1)
+            {
+                stream.BackColor = Color.Yellow;
+                return;
+            }
+
+            int streamNo;
+
+            // Check that user has entered a number
+            if(!Int32.TryParse(stream.Text, out streamNo))
+            {
+                stream.BackColor = Color.Red;
+                return;
+            }
+
+            // Check that number is in range
+            if(streamNo < 1 || streamNo > 32766)
+            {
+                stream.BackColor = Color.Red;
+                return;
+            }
+
+            // All good
+            stream.BackColor = Color.White;
+
+            // Convert number to multicast address
+            String mAddress = streamToAddress(streamNo);
+            address.Text = mAddress + ":50003";
+            mIP = IPAddress.Parse(mAddress);
+
+            /*** Set up the stream ***/
+            listen = true;
+            udpWorker = new Thread(UdpWorker);
+            udpWorker.Priority = ThreadPriority.Highest;
+            udpWorker.Start();
+
+        }
+
+        private string streamToAddress(int streamNo)
+        {
+            if (streamNo < 256)
+            {
+                return "239.255.165." + streamNo.ToString();
+            }
+
+            if (streamNo > 9471 && streamNo < 9728)
+            {
+                return "239.255.128." + (streamNo % 256).ToString();
+            }
+
+            return "239.255." + (128 + streamNo / 256).ToString() + "." + (streamNo % 256).ToString();
+
+        }
+
+        delegate void StringArgReturningVoidDelegate(string text, Control c);
+
+        private void TextSetter(string text, Control c)
+        {
+            c.Text = text;
+        }
+
+        private void SafeSet(string text, Control c)
+        {
+            this.Invoke(
+                new StringArgReturningVoidDelegate(TextSetter),
+                new object[] { text, c }
+            );
+        }
+
+        private void processPacket(byte[] receivedBytes)
+        {
+            packetCount++;
+
+            // Check header
+            String header = System.Text.Encoding.Default.GetString(receivedBytes.Skip(2).Take(4).ToArray());
+            if(!header.Equals("SVSI")) { return; }
+
+            // Get stream ID
+            int streamID = receivedBytes[6] + (receivedBytes[10] * 256);
+            SafeSet(streamID.ToString(), id);
+
+            // Check sequence IDs
+            if(lastPacketID != -2) {
+                int diff = receivedBytes[9] - lastPacketID;
+                if(diff != 1)
+                {
+                    dropCount++;
+                    Console.WriteLine("glitch (" + diff.ToString() + "), " + receivedBytes[9] + " " + lastPacketID);
+                }
+            }
+            if(receivedBytes[9] == 255)
+            {
+                lastPacketID = -1;
+            } else
+            {
+                lastPacketID = receivedBytes[9];
+            }
+
+            // Load audio samples into buffer
+            if (playing)
+            {
+                byte[] audio = receivedBytes.Skip(14).ToArray();
+                
+                // Fix endianness
+                for(int i = 0; i < audio.Length; i += 2)
+                {
+                    byte t = audio[i];
+                    audio[i] = audio[i + 1];
+                    audio[i + 1] = t;
+                }
+
+                soundBuffer.AddSamples(audio, 0, audio.Length);
+            }
+
+        }
+
+        private void UdpWorker()
+        {
+            UdpClient listener = new UdpClient();
+            listener.Client.ReceiveBufferSize = 128 * 1024;
+            listener.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            listener.ExclusiveAddressUse = false;
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 50003);
+            listener.Client.Bind(ipep);
+            listener.JoinMulticastGroup(mIP);
+            while(listen)
+            {
+                byte[] receivedBytes = listener.Receive(ref ipep);
+                processPacket(receivedBytes);
+            }
+        }
+
+        private void packupStream()
+        {
+            listen = false;
+            packets.Text = "";
+            dropped.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(!playing)
+            {
+                soundBuffer.ClearBuffer();
+                Thread.Sleep(250); // Little bit of buffer time (?)
+                sound.Play();
+            } else
+            {
+                sound.Stop();
+            }
+            playing = !playing;
+            button2.Text = playing ? "Stop" : "Play";
+            button2.BackColor = playing ? Color.Red : Color.Lime;
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            // nah
+        }
+
+    }
+}
